37c129a9b05bfcf54cd1b6ac21d430764172d809
“[mbaye98]” <mbayediop2309481@gmail.com>
2023-10-25 15:53:14 +0200

    commit after probleme from mbayeDev

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,11 +1,7 @@
 @props(['status'])
 
 @if ($status)
-<<<<<<< HEAD
     <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
-=======
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600 dark:text-green-400']) }}>
->>>>>>> a6cc9a3e3fbd9712ff4cb9b0929bacc61496597e
         {{ $status }}
     </div>
 @endif
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,11 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-<<<<<<< HEAD
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
-=======
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
->>>>>>> a6cc9a3e3fbd9712ff4cb9b0929bacc61496597e
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
